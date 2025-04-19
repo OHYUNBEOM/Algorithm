5ea67d5f40f9d2ca7d708a535cae8aef5a2ad7e6
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-04-20 00:10:03 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,30 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 int n;
-vector<int> v;
+int v[9];
 int main()
 {
 	for(int i=0;i<9;i++)
 	{
-		cin>>n;
-		v.push_back(n);
+		cin>>v[i];
 	}
-	sort(v.begin(),v.end());
+	sort(v,v+9);
 	do
 	{
 		int sum=0;
-		for(int i=0; i<7; i++)
-		{
-			sum+=v[i];
-		}
-		if(sum==100)
-		{
-			for(int i=0; i<7; i++)
-			{
-				cout<<v[i]<<"\n";
-			}
-			return 0;
-		}
-	}while(next_permutation(v.begin(),v.end()));
+		for(int i=0;i<7;i++) sum+=v[i];
+		if(sum==100)break;
+	}while(next_permutation(v,v+9));
+	for(int i=0;i<7;i++)
+	{
+		cout<<v[i]<<"\n";
+	}
 	return 0;
 }
