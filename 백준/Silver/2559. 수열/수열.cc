fc03e754cb7d9794e8e2cf5f6e792c01d6ecc868
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-04-27 17:46:28 +0900

    [Silver III] Title: 수열, Time: 24 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -5,15 +5,14 @@ int ret = -100004;
 int main()
 {
 	cin>>n>>k;
-	for(int i=1; i<=n; i++)
+	for(int i=1;i<=n;i++)
 	{
 		cin>>temp;
-		psum[i]=psum[i-1]+temp;
+		psum[i]=psum[i-1]+temp; //구간합 배열 생성
 	}
 	for(int i=k;i<=n;i++)
 	{
-		ret = max(ret,psum[i]-psum[i-k]);
-	}
-	cout<<ret<<"\n";
-	return 0;
+		ret = max(ret,psum[i] - psum[i-k]);
+	} 
+	cout<<ret;
 }
