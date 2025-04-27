f88084d4c42d57092e072a6d3537edcb94be497a
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-04-27 15:32:34 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.cc
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -1,40 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 string s;
-char temp;
-vector<char> ch;
+vector<char> v;
 int main()
 {
 	getline(cin,s);
 	for(int i=0;i<s.size();i++)
 	{
-		if(s[i]>='a'&&s[i]<='z')
+		if(s[i]>='A' && s[i]<='Z')
 		{
-			if(s[i]+13>122)
-			{
-				ch.push_back(char(s[i]+13-26));
-			}
-			else
-			{
-				ch.push_back(char(s[i]+13));
-			}
+			if(s[i]+13>90) v.push_back(s[i]-13);
+			else v.push_back(s[i]+13);
 		}
-		else if(s[i]>='A'&&s[i]<='Z')
+		else if(s[i]>='a'&& s[i]<='z')
 		{
-			if(s[i]+13>90)
-			{
-				ch.push_back(char(s[i]+13-26));	
-			}
-			else
-			{
-				ch.push_back(char(s[i]+13));
-			}
+			if(s[i]+13>122) v.push_back(s[i]-13);
+			else v.push_back(s[i]+13);
 		}
-		else
-		{
-			ch.push_back(char(s[i]));
-		}
+		else v.push_back(s[i]);
 	}
-	for(char cc : ch)cout<<cc;
-	return 0;
+	for(char vv : v)cout<<vv;
 }
