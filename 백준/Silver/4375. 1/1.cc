40d240873021b4f7346be204e930b83a17343229
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-04-29 20:38:02 +0900

    [Silver III] Title: 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.cc
+++ 백준/Silver/4375. 1/1.cc
@@ -1,27 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int n;
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
 	while(cin>>n)
 	{
-		int cnt=1,ret=1;
+		int cnt=1;
+		int ret=1;
 		while(true)
 		{
 			if(cnt%n==0)
 			{
-				cout<<ret<<"\n"<<flush;
+				cout<<ret<<"\n";
 				break;
 			}
 			else
 			{
-				cnt = (cnt*10+1);
-				cnt%=n;
+				cnt = ((cnt*10)+1)%n;
 				ret++;
 			}
 		}
-	} 
-	return 0;
+	}
 }
