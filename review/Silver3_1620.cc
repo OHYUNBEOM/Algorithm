0f957659f6176e7a1d85f731d773ccc3d0db2dd1
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-05-01 00:07:42 +0900

    Update Silver3_1620.cc

--- review/Silver3_1620.cc
+++ review/Silver3_1620.cc
@@ -1,12 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n,m;
-string s;
+string s,x;
 map<string,int> mp1;
 map<int,string> mp2;
-vector<string> v;
 int main()
 {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 	cin>>n>>m;
 	for(int i=1;i<=n;i++)
 	{
@@ -14,5 +15,16 @@ int main()
 		mp1[s]=i;
 		mp2[i]=s;
 	}
-	
+	for(int i=0;i<m;i++)
+	{
+		cin>>x;
+		if(atoi(x.c_str())==0)
+		{
+			cout<<mp1[x]<<"\n";
+		}
+		else
+		{
+			cout<<mp2[atoi(x.c_str())]<<"\n";
+		}
+	}
 }
