e2d8541883e256548c5c5ce92f7a17ff7194c678
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-05-03 14:03:38 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2108 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,m,nx,ny,xx,yy;
+int a[104][104];
+int visited[104][104];
+const int dy[4]={-1,0,1,0};
+const int dx[4]={0,1,0,-1};
+void BFS(int y,int x)
+{
+	queue<pair<int,int>> q;
+	visited[y][x]=1;
+	q.push({y,x});
+	while(q.size())
+	{
+		tie(yy,xx) = q.front(); q.pop();
+		for(int i=0;i<4;i++)
+		{
+			ny = yy + dy[i];
+			nx = xx + dx[i];
+			if(ny <0 || ny >= n || nx <0 || nx >=m || a[ny][nx]==0) continue;
+			if(visited[ny][nx]) continue;
+			visited[ny][nx] = visited[yy][xx]+1;	
+			q.push({ny,nx});
+		}
+	}
+}
+int main()
+{
+	cin>>n>>m;
+	for(int i=0;i<n;i++)
+	{
+		string line;
+		cin>>line;
+		for(int j=0;j<m;j++)
+		{
+			a[i][j] = line[j] - '0';
+		}
+	}
+	BFS(0,0);
+	cout<<visited[n-1][m-1]; // include start / if not, -1 
+}
