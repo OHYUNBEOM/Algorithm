9ecbbe2bde0ee38c2f5fe576ff13cce835c68ac8
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-05-04 15:14:19 +0900

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.cc
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -7,33 +7,24 @@ int n;
 void DFS(int y,int x,int size)
 {
 	int val=a[y][x];
-	bool same = true;
 	for(int i=y;i<y+size;i++)
 	{
 		for(int j=x;j<x+size;j++)
 		{
 			if(a[i][j]!=val)
 			{
-				same = false;
-				break;
+				int half = size/2;
+				cout<<'(';
+				DFS(y,x,half);
+				DFS(y,x+half,half);
+				DFS(y+half,x,half);
+				DFS(y+half,x+half,half);
+				cout<<')';
+				return;
 			}
 		}
-		if(!same) break;
 	}
-	
-	if(same)
-	{
-		cout<<val;
-		return;
-	}
-	
-	int half = size/2;
-	cout<<'(';
-	DFS(y,x,half);
-	DFS(y,x+half,half);
-	DFS(y+half,x,half);
-	DFS(y+half,x+half,half);
-	cout<<')';
+	cout<<val;
 }
 int main()
 {
