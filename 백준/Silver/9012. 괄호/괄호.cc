2679dbb30cc6d3e3ea7fd03650fca46e9174c8d5
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-05-06 15:50:12 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.cc
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -1,25 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int n;
 string s;
-string tp;
-int main()
-{
-	cin>>n;
-	for(int i=0;i<n;i++)
-	{
-		cin>>s;
-		stack<string> stk;
-		stk.push(string(1,s[0]));
-		for(int i=1;i<s.size();i++)
-		{
-			if(!stk.empty() && stk.top() + string(1,s[i]) == "()")
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+    cin>>n;
+    for(int t=0;t<n;t++){
+        cin>>s;
+        stack<char>stk;
+        for(char c : s) {
+            if(c=='(')
 			{
-				stk.pop();
-			}
-			else stk.push(string(1,s[i]));
-		}
-		if(stk.size()==0) cout<<"YES"<<"\n";
-		else cout<<"NO"<<"\n";
-	}
+                stk.push(c);
+            } 
+			else // c == ')'
+			{  
+                if(!stk.empty()&&stk.top()=='(') 
+				{
+                	stk.pop();
+                } 
+				else 
+				{
+                    stk.push(c);
+                }
+            }
+        }
+        cout<<(stk.empty() ? "YES" : "NO") << '\n';
+    }
+    return 0;
 }
