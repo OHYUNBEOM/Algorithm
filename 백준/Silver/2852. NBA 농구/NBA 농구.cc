b8b21660ede69af02693681a8ca0508b3b8da4e3
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-05-06 13:35:37 +0900

    [Silver III] Title: NBA 농구, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2852. NBA 농구/NBA 농구.cc
+++ 백준/Silver/2852. NBA 농구/NBA 농구.cc
@@ -1,26 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n,priv,cur_time,team_num,mm,ss,team1,team2,team1_lead_time,team2_lead_time;
+int n,priv,cur_time,t_num,mm,ss,t1,t2,t1_lead_time,t2_lead_time;
 int total_time = 48*60;
 int main()
 {
 	scanf("%d",&n);
 	for(int i=0;i<n;i++)
 	{
-		scanf("%d %2d:%2d",&team_num,&mm,&ss);
+		scanf("%d %2d:%2d",&t_num,&mm,&ss);
 		cur_time=mm*60+ss;
 		
-		if(team1>team2) team1_lead_time += cur_time-priv;
-		if(team2>team1) team2_lead_time += cur_time-priv;
-		if(team_num==1) team1++;
-		else team2++;
+		if(t1>t2) t1_lead_time += cur_time-priv;
+		if(t2>t1) t2_lead_time += cur_time-priv;
+		if(t_num==1) t1++;
+		else t2++;
 		priv=cur_time;
 	}
 	if(priv<total_time)
 	{
-		if(team1>team2) team1_lead_time += total_time-priv;
-		if(team2>team1) team2_lead_time += total_time-priv;
+		if(t1>t2) t1_lead_time += total_time-priv;
+		if(t2>t1) t2_lead_time += total_time-priv;
 	}
-	printf("%02d:%02d\n",team1_lead_time/60,team1_lead_time%60);
-	printf("%02d:%02d\n",team2_lead_time/60,team2_lead_time%60);
+	printf("%02d:%02d\n",t1_lead_time/60,t1_lead_time%60);
+	printf("%02d:%02d\n",t2_lead_time/60,t2_lead_time%60);
 }
