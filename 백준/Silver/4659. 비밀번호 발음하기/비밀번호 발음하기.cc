c5496da3ab5665528eceb74129e89a7c1745038c
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-05-15 21:53:49 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 string s;
-string v = "aeiou";
+int vowel_cnt,con_cnt;
 bool is_vowel(char c)
 {
 	if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u') return 1;
@@ -12,47 +12,40 @@ int main()
 	while(true)
 	{
 		cin>>s;
-		if(s=="end") break;
-		bool acceptable=true;
+		bool flag=true;
 		bool has_vowel=false;
-		int a=0,b=0;
+		vowel_cnt=0,con_cnt=0;
+		if(s=="end") break;
 		for(int i=0;i<s.size();i++)
 		{
-			char ss = s[i];
-			// condition 1
-			if(is_vowel(ss))
+			if(is_vowel(s[i]))
 			{
 				has_vowel=true;
-				a++;
-				b=0;
+				vowel_cnt++;
+				con_cnt=0;
 			}
 			else
 			{
-				b++;
-				a=0;
+				con_cnt++;
+				vowel_cnt=0;
 			}
 			
-
-			// condition 2
-			if(a==3 || b==3)
-			{
-				acceptable=false;
-				break;
-			}
+			//condition 2
+			if(vowel_cnt==3 || con_cnt==3) flag=false;
 			
-			// condition 3
-			if(i>=1 && s[i] == s[i-1])
+			//condition 3
+			if(i>=1 && s[i]==s[i-1])
 			{
 				if(s[i]!='e' && s[i]!='o')
 				{
-					acceptable=false;
-					break;
+					flag=false;
 				}
 			}
 		}
-		if(!has_vowel) acceptable = false;
+		//condition 1
+		if(has_vowel==false) flag=false;
 		
-		if(acceptable) cout<<"<"<<s<<"> is acceptable.\n";
-		else cout<<"<"<<s<<"> is not acceptable.\n";
+		if(flag==true) cout<<'<'<<s<<"> is acceptable.\n";
+		else cout<<'<'<<s<<"> is not acceptable.\n";
 	}
 }
