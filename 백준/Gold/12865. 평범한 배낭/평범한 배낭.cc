40191e0c9a30091ed21dc3a9ef2e0278c004de3b
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-05-17 23:19:47 +0900

    [Gold V] Title: 평범한 배낭, Time: 28 ms, Memory: 41476 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,28 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int N, K;
-int W[101], V[101];
+int n,k;
+int w[101];
+int v[101];
 int dp[101][100001];
 
 int main() {
-    cin >> N >> K;
-    for (int i = 1; i <= N; i++) {
-        cin >> W[i] >> V[i]; // i번 물건의 무게와 가치
-    }
-
-    for (int i = 1; i <= N; i++) {
-        for (int j = 0; j <= K; j++) {
-            if (j < W[i]) {
-                // 현재 물건 못 넣음
-                dp[i][j] = dp[i - 1][j];
-            } else {
-                // 넣을 수 있음 → 넣을지 말지 선택
-                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - W[i]] + V[i]);
-            }
-        }
-    }
-
-    cout << dp[N][K] << "\n";
+	cin>>n>>k;
+	for(int i=1;i<=n;i++)
+	{
+		cin>>w[i]>>v[i];
+	}
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=0;j<=k;j++)
+		{
+			if(w[i]>j)
+			{
+				dp[i][j]=dp[i-1][j];
+			}
+			else
+			{
+				dp[i][j]=max(
+					dp[i-1][j], 
+					dp[i-1][j-w[i]]+v[i]
+				);
+			}
+		}
+	}
+    cout << dp[n][k] << "\n";
     return 0;
 }
