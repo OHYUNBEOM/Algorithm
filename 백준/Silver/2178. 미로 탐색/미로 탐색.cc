29e7e1426d280ea3caabd0f2e824d933c103537e
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-05-17 09:30:11 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2108 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -2,27 +2,27 @@
 using namespace std;
 const int dy[] = {-1,0,1,0};
 const int dx[] = {0,1,0,-1};
-int n,m,ny,nx,y,x;
+int n,m,ny,nx;
 int a[104][104];
 int visited[104][104];
-void BFS(int sy, int sx)
+void BFS(int sy,int sx)
 {
+	visited[sy][sx]=1;
 	queue<pair<int,int>> q;
 	q.push({sy,sx});
-	visited[sy][sx]=1;
 	while(q.size())
 	{
-		tie(y,x) = q.front(); q.pop();
+		int y,x;
+		tie(y,x)=q.front(); q.pop();
 		for(int i=0;i<4;i++)
 		{
-			ny = y + dy[i];
-			nx = x + dx[i];
-			if(ny<0 || ny>=n || nx<0 || nx>=m) continue;
-			if(a[ny][nx]==0) continue;
-			if(!(visited[ny][nx]) && a[ny][nx]==1)
+			ny=y+dy[i];
+			nx=x+dx[i];
+			if(ny<0 || ny>=n || nx<0 || nx>=m || visited[ny][nx]) continue;
+			if(a[ny][nx]==1 && !visited[ny][nx])
 			{
-				visited[ny][nx] = visited[y][x] +1;
 				q.push({ny,nx});
+				visited[ny][nx] = visited[y][x]+1;
 			}
 		}
 	}
@@ -38,5 +38,5 @@ int main()
 		}
 	}
 	BFS(0,0);
-	cout<<visited[n-1][m-1];
+	cout<<visited[n-1][m-1]<<"\n";
 }
