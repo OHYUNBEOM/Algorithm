121830f5447fe3b3dfc58a43012dad7a9353600d
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-05-25 00:11:23 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 0 ms, Memory: 2912 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
@@ -1,41 +1,38 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int n;
-vector<int> scv;
+int n,temp;
+vector<int> v;
 int dp[61][61][61];
-
-int main() 
+int main()
 {
-   cin>>n;
-   for(int i=0;i<n;i++)
-   {
-      int temp;
-      cin>>temp;
-      scv.push_back(temp);
-   }
-    queue<tuple<int,int,int>> q;
-    q.push(make_tuple(scv[0],scv[1],scv[2]));
-
-    dp[scv[0]][scv[1]][scv[2]]=1;
-
-    vector<int> dmg = {9,3,1};
-
-    while(q.size()){
-       int a,b,c;
-        tie(a,b,c) = q.front(); q.pop();
-
-        sort(dmg.begin(), dmg.end());
-        do {
-            int na=max(0, a-dmg[0]);
-            int nb=max(0, b-dmg[1]);
-            int nc=max(0, c-dmg[2]);
-
-            if (dp[na][nb][nc] == 0) {
-                dp[na][nb][nc] = dp[a][b][c]+1;
-                q.push(make_tuple(na,nb,nc));
-            }
-        } while (next_permutation(dmg.begin(),dmg.end()));
-    }
-
-    cout << dp[0][0][0]-1 << "\n";
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		cin>>temp;
+		v.push_back(temp);
+	}
+	while(v.size()<3) v.push_back(0);
+	
+	queue<tuple<int,int,int>> q;
+	q.push(make_tuple(v[0],v[1],v[2]));
+	dp[v[0]][v[1]][v[2]]=1;
+	vector<int> dmg={9,3,1};
+	sort(dmg.begin(),dmg.end());
+	while(q.size())
+	{
+		int a,b,c;
+		tie(a,b,c)=q.front(); q.pop();
+		do
+		{
+			int na = max(0,a-dmg[0]);
+			int nb = max(0,b-dmg[1]);
+			int nc = max(0,c-dmg[2]);
+			if(dp[na][nb][nc]==0)
+			{
+				dp[na][nb][nc] = dp[a][b][c]+1;
+				q.push(make_tuple(na,nb,nc));
+			}
+		}while(next_permutation(dmg.begin(),dmg.end()));
+	}
+	cout<<dp[0][0][0]-1;
 }
