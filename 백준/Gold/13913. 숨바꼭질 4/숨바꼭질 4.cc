644c1f93f72dc73fe0ab71659ba54b680049240d
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-05-26 23:05:04 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 12 ms, Memory: 3588 KB -BaekjoonHub

---
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int n,k;
+const int MAX=100000;
+int dist[MAX+1]; // 최단 거리를 위함 
+int prevNode[MAX+1];
+int main()
+{
+	cin>>n>>k;
+	memset(dist,-1,sizeof(dist));
+	dist[n]=0;
+	prevNode[n]=-1;
+	queue<int> q;
+	q.push(n);
+	while(q.size())
+	{
+		int start=q.front();q.pop();
+		for(int next : {start-1,start+1,start*2})
+		{
+			if(next<0 || next>MAX) continue; 
+			
+			if(dist[next]==-1)
+			{
+				dist[next]=dist[start]+1;
+				prevNode[next]=start;
+				q.push(next);
+			}
+		}
+	}
+	cout<<dist[k]<<"\n";
+	vector<int> v;
+	int i = k;
+	while(i!=-1)
+	{
+		v.push_back(i);
+		i=prevNode[i];
+	}
+	reverse(v.begin(),v.end());
+	for(int vv : v)cout<<vv<<" ";
+}
