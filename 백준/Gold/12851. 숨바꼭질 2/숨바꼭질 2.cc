58050094755e4b59927708c1b27e58120a7ac328
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-05-26 21:49:04 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 0 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -1,28 +1,27 @@
 #include<bits/stdc++.h>
-using namespace std;
+using namespace std; 
+int n,k;
 const int MAX=100000;
 int dist[MAX+1];
 int cnt[MAX+1];
-int n,k;
 int main()
 {
 	cin>>n>>k;
-	queue<int> q;
 	memset(dist,-1,sizeof(dist));
 	dist[n]=0;
 	cnt[n]=1;
+	queue<int> q;
 	q.push(n);
-	while(q.size()) 
+	while(q.size())
 	{
-		int start=q.front(); q.pop();
-		int next=0;
+		int start=q.front();q.pop();
 		for(int next : {start-1,start+1,start*2})
 		{
 			if(next<0 || next>MAX) continue;
 			
 			if(dist[next]==-1)
 			{
-				dist[next]=dist[start]+1;
+				dist[next] = dist[start]+1;
 				cnt[next]+=cnt[start];
 				q.push(next);
 			}
