afa3f67f181840d8a72dcdf8e0a416331d56a2ec
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-06-04 22:33:17 +0900

    [Platinum V] Title: 숨바꼭질 5, Time: 8 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.cc
+++ 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.cc
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int MAX=500000;
 int n,k;
+const int MAX=500000;
 int visited[2][MAX+1];
 int main()
 {
@@ -11,36 +11,34 @@ int main()
 		cout<<0;
 		return 0;
 	}
-	int time=0;
 	visited[0][n]=1;
 	queue<int> q;
 	q.push(n);
+	int t=0;
 	while(true)
 	{
-		int brother = k+time*(time+1)/2;
-		if(brother>MAX) break;
-		if(visited[time%2][brother])
+		int brother=k+t*(t+1)/2;
+		if(brother<0 || brother>MAX) break;
+		if(visited[t%2][brother])
 		{
-			cout<<time;
+			cout<<t;
 			return 0;
 		}
 		int qsize=q.size();
 		for(int i=0;i<qsize;i++)
 		{
-			int cur=q.front(); q.pop();
-			for(int next : {cur+1,cur-1,cur*2})
+			int start=q.front();q.pop();
+			for(int next : {start+1,start-1,start*2})
 			{
 				if(next<0 || next>MAX) continue;
-				
-				if(!visited[(time+1)%2][next])
+				if(!visited[(t+1)%2][next])
 				{
-					visited[(time+1)%2][next]=1;
+					visited[(t+1)%2][next]=1;
 					q.push(next);
 				}
 			}
 		}
-		time++;
+		t++;
 	}
 	cout<<-1;
-	return 0;
 }
