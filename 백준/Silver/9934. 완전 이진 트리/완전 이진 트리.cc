e953972fe965ad1eacfccf322d5c1bc8ca651cff
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-06-06 22:26:52 +0900

    [Silver I] Title: 완전 이진 트리, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

--- 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.cc
+++ 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.cc
@@ -1,30 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
-int k,n,temp;
-vector<int> inorder;
-vector<vector<int>> levels;
+int k,n;
+vector<int> node;
+vector<vector<int>> level;
 void dfs(int start, int end, int depth)
 {
 	if(start>end) return;
-	int mid = (start+end)/2;
-	levels[depth].push_back(inorder[mid]);
-	dfs(start,mid-1,depth+1); // 왼쪽 서브트리 
-	dfs(mid+1,end,depth+1); // 오른쪽 서브트리 
+	int mid=(start+end)/2;
+	level[depth].push_back(node[mid]);
+	dfs(start,mid-1,depth+1);
+	dfs(mid+1,end,depth+1);
 }
 int main()
 {
 	cin>>k;
-	n=pow(2,k)-1; // 노드 갯수 
+	n=pow(2,k)-1;
 	for(int i=0;i<n;i++)
 	{
+		int temp=0;
 		cin>>temp;
-		inorder.push_back(temp); // inorder = node 목록  
+		node.push_back(temp);
 	}
-	levels.resize(k);
+	level.resize(k);
 	dfs(0,n-1,0);
-	for(int i=0;i<k;i++)
+	for(int i=0;i<level.size();i++)
 	{
-		for(int val : levels[i])
+		for(int val : level[i])
 		{
 			cout<<val<<" ";
 		}
