6dea4e728dcf63cfd07d58b93efc486239bcc0aa
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-06-07 16:07:44 +0900

    [Gold V] Title: 보물섬, Time: 108 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.cc
+++ 백준/Gold/2589. 보물섬/보물섬.cc
@@ -1,55 +1,54 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int dy[] = {-1,0,1,0};
-const int dx[] = {0,1,0,-1};
-int n,m,ret,ny,nx;
-char c;
-int a[54][54];
+const int dy[]={-1,0,1,0};
+const int dx[]={0,1,0,-1};
+int n,m,ret=INT_MIN;
+char a[54][54];
 int visited[54][54];
-vector<pair<int,int>> v;
-int BFS(int sy,int sx)
+void BFS(int sy,int sx)
 {
-	memset(visited,0,sizeof(visited));
 	visited[sy][sx]=1;
 	queue<pair<int,int>> q;
 	q.push({sy,sx});
-	int dist=0;
 	while(q.size())
 	{
 		int y,x;
-		tie(y,x)=q.front(); q.pop();
+		tie(y,x)=q.front();q.pop();
 		for(int i=0;i<4;i++)
 		{
-			ny = y+dy[i];
-			nx = x+dx[i];
-			if(ny<0 || ny>=n || nx<0 || nx>=m || visited[ny][nx] || a[ny][nx]==0) continue;
-			visited[ny][nx]=visited[y][x]+1;
-			dist=max(dist,visited[ny][nx]-1);
-			q.push({ny,nx});
+			int ny=y+dy[i];
+			int nx=x+dx[i];
+			if(ny<0 || ny>=n || nx<0 || nx>=m || visited[ny][nx] || a[ny][nx]=='W') continue;
+			if(a[ny][nx]=='L')
+			{
+				visited[ny][nx]=visited[y][x]+1;
+				ret=max(ret,visited[ny][nx]-1);
+				q.push({ny,nx});
+			}
 		}
 	}
-	return dist;
+	return;
 }
 int main()
 {
-	scanf("%d %d",&n,&m);
+	cin>>n>>m;
 	for(int i=0;i<n;i++)
 	{
 		for(int j=0;j<m;j++)
 		{
-			scanf(" %c",&c);
-			if(c=='L') 
-			{
-				a[i][j]=1;
-				v.push_back({i,j});
-			}
-			else if(c=='W') a[i][j]=0;
+			cin>>a[i][j];
 		}
 	}
-	for(auto it : v)
+	for(int i=0;i<n;i++)
 	{
-		int dist=BFS(it.first,it.second);
-		ret=max(ret,dist);
+		for(int j=0;j<m;j++)
+		{
+			if(a[i][j]=='L')
+			{
+				memset(visited,0,sizeof(visited));
+				BFS(i,j);
+			}
+		}
 	}
-	printf("%d",ret);
+	cout<<ret;
 }
