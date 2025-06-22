fcc4f9dc62fc81f56fc01f81f3d4315c5bd3f814
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-06-22 23:50:02 +0900

    [level 2] Title: 타겟 넘버, Time: 4.27 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -1,17 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-void dfs(int idx,int sum, vector<int>& numbers, int target, int& answer)
+vector<int> v;
+int tgt,ans;
+void dfs(int idx,int sum)
 {
-    if(idx==numbers.size())
+    if(idx==v.size())
     {
-        if(sum==target) answer++;
+        if(sum==tgt) ans++;
         return;
     }
-    dfs(idx+1,sum+numbers[idx],numbers,target,answer);
-    dfs(idx+1,sum-numbers[idx],numbers,target,answer);
+    dfs(idx+1,sum+v[idx]);
+    dfs(idx+1,sum-v[idx]);
 }
 int solution(vector<int> numbers, int target) {
-    int answer=0;
-    dfs(0,0,numbers,target,answer); //idx,sum
-    return answer;
+    v=numbers;
+    tgt=target;
+    ans=0;
+    dfs(0,0);
+    return ans;
 }
