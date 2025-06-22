5560554b43d836603886f35834f56717afce9064
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-06-22 13:39:06 +0900

    Create re2_Silver1_9934.cc

---
+++ review/re2_Silver1_9934.cc
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int k;
+vector<int> node;
+vector<vector<int>> level;
+void dfs(int start,int end,int depth)
+{
+	if(start>end) return;
+	int mid=(start+end)/2;
+	level[depth].push_back(node[mid]);
+	dfs(start,mid-1,depth+1);
+	dfs(mid+1,end,depth+1);
+}
+int main()
+{
+	cin>>k;
+	int n=pow(2,k)-1;
+	for(int i=0;i<n;i++)
+	{
+		int temp;
+		cin>>temp;
+		node.push_back(temp);
+	}
+	level.resize(k);
+	dfs(0,n-1,0);
+	for(int i=0;i<level.size();i++)
+	{
+		for(auto it : level[i])
+		{
+			cout<<it<<" ";
+		}
+		cout<<"\n";
+	}
+}
