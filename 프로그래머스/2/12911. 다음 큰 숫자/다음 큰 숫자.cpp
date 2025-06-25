899f375b96c8f8a102d6b9be98c7515d3c141bf2
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-06-25 21:40:29 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 3.76 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
@@ -1,26 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-int change(int num)
-{
-    string result="";
-    while(num>0)
-    {
-        result = to_string(num%2)+result;
-        num=num/2;
-    }
-    int count=0;
-    for(int i=0;i<result.size();i++)
-    {
-        if(result[i]=='1') count++;
-    }
-    return count;
-}
 int solution(int n) {
-    int count_n = change(n); // n -> 2진수로 변환, 1의 갯수 저장
-    int temp=n+1; // n+1부터 검사
-    while(true)
-    {
-        if(change(temp)==count_n) return temp;
-        else temp++;
-    }
+    int num=bitset<20>(n).count();
+    while(bitset<20>(++n).count() != num);
+    return n;
 }
