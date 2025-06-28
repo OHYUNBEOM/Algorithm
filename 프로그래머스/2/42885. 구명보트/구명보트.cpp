41b671ef29e6bef22d2f328520188a42363579d3
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-06-28 15:31:25 +0900

    [level 2] Title: 구명보트, Time: 1.94 ms, Memory: 4.63 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.cpp
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -5,21 +5,13 @@ int solution(vector<int> people, int limit) {
     int cnt=0;
     int left=0;
     int right=people.size()-1;
-    int temp=limit;
     while(left<=right)
     {
-        limit=temp;
         if(people[left]+people[right]<=limit)
         {
-            limit=limit-(people[left]+people[right]);
             left++;
-            right--;
         }
-        else
-        {
-            limit-=people[right];
-            right--;
-        }
+        right--;
         cnt++;
     }
     return cnt;
