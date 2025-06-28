b963f71e31cc4cc05f1ae78fb64e35aa7e70d0f7
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-06-28 13:55:31 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -6,8 +6,8 @@ int solution(int n)
     int cnt=0;
     while(n>0)
     {
-        if(n%2==0) n/=2;
-        else
+        if(n%2==0) n/=2; // 순간이동이 가능하다면 최대한 나눠줌
+        else // 최대한 나눈 이후 점프하면서 점프 거리 카운팅
         {
             n-=1;
             cnt++;
