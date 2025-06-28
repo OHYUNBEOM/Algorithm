c3bc167ae5b477399e76bcc40fff3093f806b1b8
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-06-28 13:49:09 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 3.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solution(int n)
+{
+    int cnt=0;
+    while(n>0)
+    {
+        if(n%2==0) n/=2;
+        else
+        {
+            n-=1;
+            cnt++;
+        }
+    }
+    return cnt;
+}
