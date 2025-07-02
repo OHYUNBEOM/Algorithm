e59a9abe305154c5e69b61ac1e2cd404089fb813
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-07-02 22:14:41 +0900

    Update 피로도.cpp

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -1,9 +1,25 @@
-#include <string>
-#include <vector>
-
+#include<bits/stdc++.h>
 using namespace std;
-
+int visited[10];
+int n,ret=INT_MIN;
+vector<vector<int>> global_dungeons;
+void go(int k, int cnt)
+{
+    ret=max(ret,cnt);
+    for(int i=0;i<n;i++)
+    {
+        if(!visited[i] && k>=global_dungeons[i][0])
+        {
+            visited[i]=true;
+            go(k-global_dungeons[i][1],cnt+1);
+            visited[i]=false;
+        }
+    }
+}
 int solution(int k, vector<vector<int>> dungeons) {
-    int answer = -1;
-    return answer;
+    n = dungeons.size();
+    global_dungeons=dungeons;
+    memset(visited,0,sizeof(visited));
+    go(k,0);
+    return ret;
 }
