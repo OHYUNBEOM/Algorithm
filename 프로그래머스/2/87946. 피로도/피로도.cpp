845044ff9ac2ac66de3ed44111dd52073720023b
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-07-02 22:10:21 +0900

    [level 2] Title: 피로도, Time: 0.81 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -1,30 +1,9 @@
-#include<bits/stdc++.h>
+#include <string>
+#include <vector>
+
 using namespace std;
-int visited[10];
-int n,ret=INT_MIN;
-vector<vector<int>> global_dungeons;
-void go(int k)
-{
-    int cnt=0;
-    for(int i=0;i<n;i++)
-    {
-        if(visited[i]) cnt++;
-    }
-    ret=max(ret,cnt);
-    for(int i=0;i<n;i++)
-    {
-        if(!visited[i] && k>=global_dungeons[i][0])
-        {
-            visited[i]=true;
-            go(k-global_dungeons[i][1]);
-            visited[i]=false;
-        }
-    }
-}
+
 int solution(int k, vector<vector<int>> dungeons) {
-    n = dungeons.size();
-    global_dungeons=dungeons;
-    memset(visited,0,sizeof(visited));
-    go(k);
-    return ret;
+    int answer = -1;
+    return answer;
 }
