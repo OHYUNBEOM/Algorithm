a178d294b864a18fd5a8ec6d279f36e3635b7833
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-07-03 13:38:45 +0900

    [level 2] Title: [1차] 캐시, Time: 19.36 ms, Memory: 12.5 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.cpp
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.cpp
@@ -1,8 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 int solution(int cacheSize, vector<string> cities) {
-    int ret=0;
-    int n=cities.size();
+    int ret=0; // 실행 시간
+    int n=cities.size(); // 도시들
     for(int i=0;i<n;i++)
     {
         for(int j=0;j<cities[i].size();j++)
@@ -10,26 +10,26 @@ int solution(int cacheSize, vector<string> cities) {
             cities[i][j]=tolower(cities[i][j]);
         }
     }
-    list<string> lst; // cache를 담을 자료구조
+    list<string> lst;
     for(int i=0;i<n;i++)
     {
-        auto it = find(lst.begin(),lst.end(),cities[i]); // cache에서 못찾으면 lst.end() 반환
-        if(it!=lst.end()) // cache hit
+        auto it=find(lst.begin(),lst.end(),cities[i]);
+        if(it!=lst.end()) //cache hit
         {
+            ret+=1;
             lst.erase(it);
             lst.push_back(cities[i]);
-            ret+=1;
         }
         else // cache miss
         {
             ret+=5;
             if(cacheSize>0)
             {
-                if(lst.size()==cacheSize) // cache miss가 났고 cache가 꽉 찼다면
+                if(lst.size()==cacheSize)
                 {
-                    lst.pop_front(); // 가장 오래된 cache 도시 삭제
+                    lst.pop_front();
                 }
-                lst.push_back(cities[i]); // 순서 중요, cache 다 찼으면 앞에 하나 비워주고 push_back
+                lst.push_back(cities[i]);
             }
         }
     }
