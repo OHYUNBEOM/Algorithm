7d937a5a1d2c79f740feaeb44db965ed376f119c
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-07-05 23:41:35 +0900

    [level 2] Title: 모음 사전, Time: 0.12 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+const char vowel[5] = {'A','E','I','O','U'};
+int ret,cnt;
+void dfs(string cur,string target)
+{
+    if(cur==target)
+    {
+        ret=cnt;
+        return;
+    }
+    
+    if(cur.length()==5) return;
+    
+    for(int i=0;i<5;i++)
+    {
+        cnt++;
+        dfs(cur+vowel[i],target);
+        if(ret) return;
+    }
+}
+int solution(string word) {
+    dfs("",word);
+    return ret;
+}
