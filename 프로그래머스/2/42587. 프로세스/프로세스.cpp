8adc2846cf5d86a16025496c5dcabb9f0d399dce
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-07-05 12:48:25 +0900

    [level 2] Title: 프로세스, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.cpp
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -1,39 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 int solution(vector<int> priorities, int location) {
-    int n=priorities.size();
-    queue<pair<int,int>> q;
-    for(int i=0;i<n;i++)
+    queue<int> q;
+    vector<int> v;
+    for(int i=0;i<priorities.size();i++)
     {
-        q.push({priorities[i],i}); //priorities[i] : 우선순위, i : index(location)
+        q.push(i); // index만 순서대로 저장
     }
-    int ret=0;
     while(q.size())
     {
-        auto cur=q.front(); q.pop();
-        bool high=false;
-        queue<pair<int,int>> temp=q; // 복사본으로 검사
-        while(temp.size())
+        int n=q.front();q.pop();
+        // 우선순위가 더 높은 프로세스가 있는 경우
+        if(priorities[n]!=*max_element(priorities.begin(),priorities.end()))
         {
-            if(temp.front().first>cur.first)
-            {
-                high=true;
-                break;
-            }
-            temp.pop();
+            q.push(n);
         }
-        if(high) // 우선순위가 더 높은놈이 있는 경우
+        else // 우선순위가 가장 높은 경우
         {
-            q.push(cur); // 뒤로 보냄
-        }
-        else // 우선순위가 더 높은놈이 없는 경우
-        {
-            ret++;
-            if(cur.second==location)
-            {
-                return ret;
-            }
+            v.push_back(n); // v에 index 번호 삽입
+            priorities[n]=0; // 프로세스 실행한 경우 0으로 변경
         }
     }
-    return ret;
+    for(int i=0;i<v.size();i++)
+    {
+        if(v[i]==location) return i+1;
+    }
 }
