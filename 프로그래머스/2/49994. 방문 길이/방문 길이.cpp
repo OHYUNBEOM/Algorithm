c83a31bc0f870251c0f783304252c3748f7c8cb2
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-07-07 19:13:43 +0900

    [level 2] Title: 방문 길이, Time: 0.13 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.cpp
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.cpp
@@ -2,19 +2,21 @@
 using namespace std;
 int solution(string dirs) {
     set<tuple<int,int,int,int>> visited;
-    int y=0,x=0;
+    int y=0;
+    int x=0;
     for(char c : dirs)
     {
-        int ny=y,nx=x;
+        int ny=y;
+        int nx=x;
         if(c=='U') ny++;
         if(c=='D') ny--;
-        if(c=='R') nx++;
         if(c=='L') nx--;
-        if(ny<-5 || ny>5 || nx<-5 || nx>5) continue;
+        if(c=='R') nx++;
+        if(nx<-5 || nx>5 || ny<-5 || ny>5) continue;
         visited.insert({y,x,ny,nx});
-        visited.insert({ny,nx,y,x}); // 한번 가본 길 = 반대로 와도 가본 길이므로 반대도 처리해줌
-        y=ny; // 한칸 옮겨간 곳에서 이어서 탐색
+        visited.insert({ny,nx,y,x});
+        y=ny;
         x=nx;
     }
-    return visited.size()/2; // 반대로 온 경우도 포함했으므로 2 나누고 return
+    return visited.size()/2;
 }
