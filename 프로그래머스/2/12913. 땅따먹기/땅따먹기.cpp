4df3a93b96a07e226cab65550d7a2ec178fd0a6d
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-07-15 21:45:40 +0900

    [level 2] Title: 땅따먹기, Time: 7.44 ms, Memory: 27.4 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
@@ -1,13 +1,25 @@
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h>
 using namespace std;
-
-int solution(vector<vector<int> > land)
+int dp[100004][4];
+int solution(vector<vector<int>> land)
 {
-    int answer = 0;
-
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    cout << "Hello Cpp" << endl;
-
-    return answer;
+    int n = land.size();
+    for(int i=0;i<4;i++)
+    {
+        dp[0][i]=land[0][i];
+    }
+    for(int i=1;i<n;i++)
+    {
+        for(int j=0;j<4;j++)
+        {
+            int prev=0;
+            for(int k=0;k<4;k++)
+            {
+                if(j==k) continue;
+                prev=max(prev,dp[i-1][k]);
+            }
+            dp[i][j]=land[i][j]+prev;
+        }
+    }
+    return *max_element(dp[n-1],dp[n-1]+4);
 }
