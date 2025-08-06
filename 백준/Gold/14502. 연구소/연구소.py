50d080b18d81025cbe25c74adc4cceb87a407ebd
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-08-06 15:50:09 +0900

    [Gold IV] Title: 연구소, Time: 2828 ms, Memory: 33568 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,6 +1,8 @@
-from itertools import combinations
+import sys
 import copy
-
+input=sys.stdin.readline
+sys.setrecursionlimit(10**6)
+from itertools import combinations
 dy=[-1,0,1,0]
 dx=[0,1,0,-1]
 
@@ -9,12 +11,13 @@ safe=[]
 
 n,m=map(int,input().split())
 a=[[0]*m for _ in range(n)]
+
 for i in range(n):
     line=list(map(int,input().split()))
     for j in range(m):
         a[i][j]=line[j]
         if a[i][j]==0:
-            safe.append((i,j))
+            safe.append((i,j)) # 벽 세울 장소 후보
 
 def dfs(y,x):
     visited[y][x]=True
@@ -23,19 +26,17 @@ def dfs(y,x):
         nx=x+dx[i]
         if ny<0 or ny>=n or nx<0 or nx>=m or visited[ny][nx] or a_copy[ny][nx]==1:
             continue
-        if a_copy[ny][nx]==0:
+        if a_copy[ny][nx]==0 and not visited[ny][nx]:
             visited[ny][nx]=True
             a_copy[ny][nx]=2
             dfs(ny,nx)
 
-
-for wall in combinations(safe,3): # 안전 영역 중 3곳을 고름
-    a_copy=copy.deepcopy(a) # 조합 별 확인을 위해 원본 a는 보존, a_copy 사용
-    visited=[[False]*m for _ in range(n)]
+for wall in combinations(safe,3): # 0인 곳에서 3개 벽 세움
+    a_copy=copy.deepcopy(a)
+    visited = [[False] * m for _ in range(n)]
     cnt=0
-    
-    for y,x in wall: # 벽 세우기
-        a_copy[y][x]=1
+    for y,x in wall:
+        a_copy[y][x]=1 # 벽 세우기
 
     for i in range(n):
         for j in range(m):
