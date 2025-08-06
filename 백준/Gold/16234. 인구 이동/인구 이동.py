4af809bc04f7c9b03fd163c2c04de769965ffd1e
YunBeomOh <66807406+OHYUNBEOM@users.noreply.github.com>
2025-08-06 23:40:13 +0900

    [Gold IV] Title: 인구 이동, Time: 3868 ms, Memory: 33736 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -21,15 +21,15 @@ def go(y,x):
         if l <= abs(a[y][x]-a[ny][nx]) <= r:
             go(ny,nx)
 
-while True:
-    flag=True
+while True: # 연합이 존재하는 한 계속 진행
+    flag=True # 연합 존재 유무 체크
     visited = [[False] * n for _ in range(n)]
     for i in range(n):
         for j in range(n):
-            if not visited[i][j]:
+            if not visited[i][j]: 
                 united=[]
                 go(i,j) # 연합 가능한지 확인
-                if len(united)>1:
+                if len(united)>1: # 2개 이상부터 연합
                     flag=False
                     total=avg=0
 
@@ -41,9 +41,9 @@ while True:
                     for y,x in united:
                         a[y][x]=avg
 
-    if flag:
+    if flag: # flag가 변하지 않았다 = 연합이 없었다
         break
-    else:
+    else: # flag가 변했다 = 연합이 존재해서 국경 이동을 했다
         ret+=1
 
 print(ret)
